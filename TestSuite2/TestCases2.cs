3afb828418e649d7d115aa6480f1e3d57ba297b2
Valrravn <37902124+Valrravn@users.noreply.github.com>
2024-08-05 11:20:31 +0200

    Add test ignore service message

--- TestSuite2/TestCases2.cs
+++ TestSuite2/TestCases2.cs
@@ -5,6 +5,7 @@ namespace TestSuite2
     {
         [TestMethod]
         public void FailingTest() {
+            Console.WriteLine("##teamcity[testIgnored name='FailingTest' message='Ignored from Service Message']");
             Assert.AreEqual(1, 0, "This test always fails");
         }
     }
