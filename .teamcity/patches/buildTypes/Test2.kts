05b5ca63c2fb4a137cbf8638a6f7a11e5078f317
Valravn <Valravn@TeamCity>
2024-08-05 11:15:33 +0200

    TeamCity change in 'Pipeline Tutorial - .NET 8 Chain' project: runners of 'Test2' build configuration were updated

--- .teamcity/patches/buildTypes/Test2.kts
+++ .teamcity/patches/buildTypes/Test2.kts
@@ -4,7 +4,6 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.DotnetTestStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.csharpScript
 import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetTest
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -24,19 +23,11 @@ changeBuildType(RelativeId("Test2")) {
         }
     }
     steps {
-        insert(0) {
-            script {
-                name = "Disk prep"
-                id = "simpleRunner"
-                scriptContent = "test"
-                param("teamcity.step.phase", "bootstrap")
-            }
-        }
-        update<DotnetTestStep>(1) {
+        update<DotnetTestStep>(0) {
             executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
             clearConditions()
         }
-        insert(2) {
+        insert(1) {
             csharpScript {
                 name = "mute test"
                 id = "mute_test"
